b35e09ed7dac9b0afea98f089ab0463784639f43
anthony-chang <anthony20093@gmail.com>
2020-09-04 17:36:10 -0400

    Clean up, add comments

--- buddy_alloc.c
+++ buddy_alloc.c
@@ -11,63 +11,68 @@ void* balloc(size_t sz) {
     int i = 0, order;
     void *block, *buddy;
 
+     /*
+      * Get i as least integer such that i >= log(sz + 1),
+      * with 1 extra byte to store order (prefixed in
+      * memory)
+      */
     for(; BLOCK_SZ(i) < sz + 1; ++i);
-    i = (i < MIN_ORDER) ? MIN_ORDER : i;
+
+    i = (i < MIN_ORDER) ? MIN_ORDER : i; // need 16 bytes
     order = i;
 
     for(;; ++i) {
         if(i > MAX_ORDER)
             return NULL;
-        if(ctx_p->freeList[i])
+        if(ctx_p->freeList[i]) // get first available block in level
             break;
     }
 
-    block = ctx_p->freeList[i];
-    ctx_p->freeList[i] = *(void**)ctx_p->freeList[i];
+    block = ctx_p->freeList[i]; //get mem block
+    ctx_p->freeList[i] = *(void**)ctx_p->freeList[i]; // set mem block to allocated
 
-    while(i-- > order) {
+    while(i-- > order) { // merge free buddies up to tree root
         buddy = GET_BUDDY(block, i);
         ctx_p->freeList[i] = buddy;
     }
 
-    *((uint8_t*)(block - 1)) = order;
+    *((uint8_t*)(block - 1)) = order; // prefix mem with order for dealloc later
     return block;
 }
 
-void bfree(void* block) {
+void bdealloc(void* block) {
     int i = 0;
     void* buddy;
     void** pp;
 
-    i = *((uint8_t*)(block - 1));
+    i = *((uint8_t*)(block - 1)); // fetch order
 
     for(;; ++i) {
         buddy = GET_BUDDY(block, i);
         pp = &ctx_p->freeList[i];
 
-        while(*pp && *pp != buddy) {
+        while(*pp && *pp != buddy) { // loop through level
             pp = (void**) *pp;
         }
 
-        if(*pp != buddy) {
+        if(*pp != buddy) { // if buddy not found in level, add it in
             *(void**) block = ctx_p->freeList[i];
             ctx_p->freeList[i] = block;
             return;
         }
+        else {
+            if(block >= buddy) // always merge to lower block
+                block = buddy;
 
-        if(block >= buddy)
-            block = buddy;
-
-        *pp = *(void**)*pp;
+            *pp = *(void**)*pp; // remove block
+        }
     }
 }
-
 
 void buddy_init(buddy_t* buddy) {
     ctx_p = buddy;
     memset(ctx_p, 0, sizeof(buddy_t));
-    ctx_p->freeList[MAX_ORDER] = ctx_p->pool;
-    printf("pool=%p\n", ctx_p->pool);
+    ctx_p->freeList[MAX_ORDER] = ctx_p->mem; // starting addr
 }
 
 void buddy_term(buddy_t* buddy) {
@@ -78,14 +83,15 @@ int main(int argc, const char** argv) {
     buddy_t* buddy = malloc(sizeof(buddy_t));
     buddy_init(buddy);
 
-    int* test = (int*)balloc(sizeof(int));
+    void* test = balloc(sizeof(int));
+    void* a = balloc(sizeof(char));
     printf("%p, sz=%" PRIu8 "\n", test, BLOCK_SZ(*((uint8_t*)(test - 1))));
 
-    *test = 5;
-    bfree(test);
+    bdealloc(test);
+    bdealloc(a);
 
+    buddy_term(buddy);
+    free(buddy);
 
-
-    printf("hello there, %u\n", POOL_SZ);
     return 0;
 }
--- buddy_alloc.h
+++ buddy_alloc.h
@@ -6,27 +6,25 @@
 #define MAX_ORDER             14
 #define MIN_ORDER             4
 
-#define POOL_SZ              (1 << MAX_ORDER)
-
 #define BLOCK_SZ(n)          (1 << n)
 
-#define _BASE_ADDR_          ((uintptr_t)ctx_p->pool)
+#define _BASE_ADDR_          ((uintptr_t)ctx_p->mem)
 #define _OFFSET_(addr)       ((uintptr_t)addr - _BASE_ADDR_)
 #define _GET_BUDDY_(addr, n) (_OFFSET_(addr) ^ (1 << n))
 #define GET_BUDDY(addr, n)   ((void*)( _GET_BUDDY_(addr, n) + _BASE_ADDR_))
 
 typedef struct buddy {
     void* freeList [MAX_ORDER + 2];
-    uint8_t pool[POOL_SZ];
+    uint8_t mem [1 << MAX_ORDER];
 } buddy_t;
 
-buddy_t* ctx_p = 0;
+buddy_t* ctx_p;
 
 void buddy_init(buddy_t* buddy);
 void buddy_term(buddy_t* buddy);
 
 void* balloc(size_t sz);
-void bfree(void* block);
+void bdealloc(void* block);
 
 
 #endif // BUDDY_ALLOC_H
